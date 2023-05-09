761032672e3af74388770d49ef3aee7055a1ee82
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2023-05-09 12:19:22 -0400

    modified single program to link to relevant events

--- templates/single-program.php
+++ templates/single-program.php
@@ -35,6 +35,68 @@ while (have_posts()) {
         </div>
         <div class="generic-content">
             <?php the_content(); ?>
+            <?php
+            //Shows posts where the event date is great than or equual to today's date.
+            $today = date('Ymd');
+            $args = array(
+                'posts_per_page' => 4,
+                "post_type" => "event",
+                "meta_key" => "event_date",
+                "orderby" => "meta_value_num",
+                "order" => "ASC",
+                "meta_query" => array(
+                    array(
+                        'key' => 'event_date',
+                        'compare' => '>=',
+                        'value' => $today,
+                        'type' => 'numeric'
+                    ),
+                    array( //filtering on ACF - related
+                        'key' => 'related_programs',
+                        'compare' => 'LIKE',
+                        'value' => '"' . get_the_ID() . '"'
+                    )
+                )
+            );
+            $homepageEvents = new WP_Query($args);
+
+            while ($homepageEvents->have_posts()) {
+                $homepageEvents->the_post();
+            ?>
+
+                <div class="event-summary">
+                    <a class="event-summary__date event-summary__date t-center" href="<?php the_permalink(); ?>">
+                        <span class="event-summary__month"> <?php
+                                                            $eventDate = new DateTime(get_field('event_date'));
+                                                            echo $eventDate->format('M');
+                                                            ?>
+                        </span>
+                        <span class="event-summary__day"><?php
+
+                                                            echo $eventDate->format('d');
+
+                                                            ?></span>
+                    </a>
+                    <div class="event-summary__content">
+                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
+                        <?php
+                        if (has_excerpt()) {
+                        ?>
+                            <p><?php echo get_the_excerpt() ?><a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p>
+                        <?php
+                        } else {
+                        ?>
+                            <p><?php echo wp_trim_words(get_the_content(), 12, null) ?><a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        <?php
+                        }
+                        ?>
+
+                    </div>
+                </div>
+            <?php }
+            wp_reset_postdata();
+
+            ?>
         </div>
     </div>
 
